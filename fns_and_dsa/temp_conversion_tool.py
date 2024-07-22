24a75ccddc7add05c25a141d11de3e71f47344ff
Inetanbor Osatufo Elijah <elijahosas@gmail.com>
2024-07-22 16:02:43 +0100

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,7 +1,7 @@
 # Define global conversion factors
-global FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-global CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
-CELSIUS_TO_FAHRENHEIT_FACTOR\s*=\s*9\/5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
 def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
