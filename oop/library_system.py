73621ac4941ec093cb98840b5560299a77b66d38
Elijah Inetanbor <elijahosas@gmail.com>
2024-07-28 23:12:31 +0100

    Made some corrections

--- oop/library_system.py
+++ oop/library_system.py
@@ -12,7 +12,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"{super().__str__()} - {self.file_size}MB"
+        return f"{super().__str__()} - {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
@@ -20,7 +20,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"{super().__str__()} - {self.page_count} pages"
+        return f"{super().__str__()} - {self.page_count}"
 
 class Library:
     def __init__(self):
